049447c83fd03288fe4baf749e21d50da117fcf3
jazevedo6 <jazevedo6@gatech.edu>
2018-10-19 23:12:55 -0400

    Responses not recieved

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
 var request = require('ajax-request');
+
+var temp_course_id = "CS";
+function scrape(course_id) {
+    request({
+        url: 'https://www.coursicle.com/gatech/search.php',
+        dataType: 'text',
+        method: 'POST',
+        headers: { origin: 'https://www.coursicle.com', referer: 'https://www.coursicle.com/gatech/' },
+        data: {
+            subject: course_id, 
+            courseNumber:'', 
+            courseNumberInequality: '', 
+            attributes : '', 
+            minGenEds : '', 
+            title : '', 
+            days:'', 
+            startTime: '', 
+            endTime: '', 
+            instructor:'', 
+            whatPercentAs:'40', 
+            atLeastSomePercentAs: 'false', 
+            semester:'spring2019',
+            isAddingMore: 'false'
+        }, 
+    }, function(err, res, body) {
+        console.log(body);
+    });
+}
+
+scrape(temp_course_id);
