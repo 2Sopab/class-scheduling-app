8aa0487896f9471e746e565f3d03320f40537aca
Patrick Liu <patrickliu2000@gmail.com>
2018-10-19 23:46:55 -0400

    courseoff initial commit

--- main.js
+++ main.js
@@ -1,31 +1,11 @@
-var request = require('ajax-request');
+// function setup() {
+//     loadJSON('https://soc.courseoff.com/gatech/terms/201808/majors/MATH/courses/1554/sections',gotData);
+// }
 
-var temp_course_id = "CS";
-function scrape(course_id) {
-    request({
-        url: 'https://www.coursicle.com/gatech/search.php',
-        dataType: 'text',
-        method: 'POST',
-        headers: { origin: 'https://www.coursicle.com', referer: 'https://www.coursicle.com/gatech/' },
-        data: {
-            subject: course_id, 
-            courseNumber:'', 
-            courseNumberInequality: '', 
-            attributes : '', 
-            minGenEds : '', 
-            title : '', 
-            days:'', 
-            startTime: '', 
-            endTime: '', 
-            instructor:'', 
-            whatPercentAs:'40', 
-            atLeastSomePercentAs: 'false', 
-            semester:'spring2019',
-            isAddingMore: 'false'
-        }, 
-    }, function(err, res, body) {
-        console.log(body);
-    });
-}
+// function gotData(data) {
+//     console.log(data);
+// }
 
-scrape(temp_course_id);
+// setup()
+
+const 
