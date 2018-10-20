02e32a3de34210b41ecfbcfd3d73d65157a24260
Patrick Liu <patrickliu2000@gmail.com>
2018-10-20 00:48:07 -0400

    added looping example and added more dependencies

--- main.js
+++ main.js
@@ -2,7 +2,11 @@ let fetch = require('node-fetch')
 let getData = async () => {
     let response = await fetch('https://soc.courseoff.com/gatech/terms/201808/majors/MATH/courses') //add any request url from courseoff here
     let data = await response.json();
-    console.log(data);
+    //loop through data
+    data.forEach(element => {
+        console.log(element);
+    });
+    //console.log(data);
 }
 getData().catch((err) => {
     console.log(err);
