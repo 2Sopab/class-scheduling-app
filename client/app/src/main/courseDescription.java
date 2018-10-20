b4f878475568fbc384629e7648ec9ab70e38a83b
Gabriel Wang <43443158+gabriel-q-wang@users.noreply.github.com>
2018-10-20 09:14:45 -0400

    Add files via upload
    Schedule view of classes
    Onclick, leads to course description
    badly written

---
+++ client/app/src/main/courseDescription.java
@@ -0,0 +1,15 @@
+package com.example.gwgwa.testing3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class courseDescription extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course_description);
+    }
+}
