50d6a9d8ce863fb5a3c06a0c8ff731740ed6ab23
Patrick Liu <patrickliu2000@gmail.com>
2018-10-20 18:31:44 -0400

    added async await parsing of json

--- main.js
+++ main.js
@@ -19,30 +19,9 @@ let mysql = require('mysql');
 let fs = require('fs');
 let express = require('express');
 let app = express();
-
+let fetch = require('node-fetch')
 let majorArr = [];
-// app.get('/', (req, res) => res.json(res));
-// app.listen(3000, () => console.log('Example app listening on port 3000!'));
-
-//gets all terms
-request.get('https://soc.courseoff.com/gatech/terms', (error, response, body) => {
-    if (error != null) {
-        console.log(error);
-    }
-    let jsonData = JSON.parse(body);
-    jsonData.forEach(element => {
-        console.log(element);
-    });
-});
-
-//gets Spring semester
-request.get('https://soc.courseoff.com/gatech/terms/201901', (error, response, body) => {
-    if (error != null) {
-        console.log(error);
-    }
-    let jsonData = JSON.parse(body);
-    console.log(jsonData);
-});
+let majorCourseMap = new Map()
 
 /**
  * gets all majors-has ident(abbreviation)and name(full subject name) fields;
@@ -58,7 +37,21 @@ request.get('https://soc.courseoff.com/gatech/terms/201901/majors', (error, resp
         console.log(element);
     });
 });
+setTimeout((() => {
+    getCourses().then((majorArr) => {
 
+    }).catch((err) => {
+        console.log(err);
+    }
+}), 2000)
+let getCourses = async (majors) => {
+    majors.forEach((element) => {
+        let response = await fetch(`https://soc.courseoff.com/gatech/terms/201901/majors/${element}/courses`)
+        let courses = await response.json();
+        //courses for specific major-element param
+        majorCourseMap.push(element, courses);
+    })
+}
 //loops through all majors and gets their courses, waits 5 secs for all majors to be pushed into array
 setTimeout((() => {
     for (i = 0; i < majorArr.length; i++) {
@@ -73,12 +66,11 @@ setTimeout((() => {
                     if (error != null) {
                         console.log(error);
                     }
-                    let jsonData = JSON.parse(body);
-                    jsonData.forEach(element => {
-                        console.log(element);
+                    let sectionData = JSON.parse(body);
+                    sectionData.forEach(elem => {
+                        console.log(elem);
                     });
                 });
-                console.log(element.ident);
             });
         });
     }
