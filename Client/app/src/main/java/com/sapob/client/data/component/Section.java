84232d6bf6d7c0d3c02dad41380d4b9d1256b266
jazevedo6 <jazevedo6@gatech.edu>
2018-10-20 20:05:02 -0400

    Removed merge conflict artifacts

--- Client/app/src/main/java/com/sapob/client/data/component/Section.java
+++ Client/app/src/main/java/com/sapob/client/data/component/Section.java
@@ -1,36 +1,23 @@
 package com.sapob.client.data.component;
 
-<<<<<<< HEAD
-=======
 import java.util.List;
 import java.util.ArrayList;
 
->>>>>>> 0dfee5b0307c610fe463de768c04769cfc29faca
 public class Section {
     private String courseName;
     private List<Integer> CRN;
     private Professor prof;
     private int remainingSpace;
-<<<<<<< HEAD
     private Block block;
-
-    public Section(Course courseName, int CRN, Professor prof, int remainingSpace, Block block) {
-=======
-    private List<Block> blocks;
     private int rating;
 
-    public Section(String courseName, ArrayList<Integer> CRN, Professor prof, int remainingSpace, List<Block> blocks, int rating) {
->>>>>>> 0dfee5b0307c610fe463de768c04769cfc29faca
+    public Section(String courseName, ArrayList<Integer> CRN, Professor prof, int remainingSpace, Block block, int rating) {
         this.courseName = courseName;
         this.CRN = CRN;
         this.prof = prof;
         this.remainingSpace = remainingSpace;
-<<<<<<< HEAD
         this.block = block;
-=======
-        this.blocks = blocks;
         this.rating = rating;
->>>>>>> 0dfee5b0307c610fe463de768c04769cfc29faca
     }
     
     public String getCourseName() {
